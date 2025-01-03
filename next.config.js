321e1defb03387843b8a10ac60576b6607cb744d
Emir <52050284+emirthab@users.noreply.github.com>
2025-01-03 19:33:07 +0300

    db

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env:{
+    DATABASE_URL: "postgresql://postgres:RswOYGzrtYEdkGiuvlgOJKWGxcvQaUFe@monorail.proxy.rlwy.net:36370/railway",
+  },
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
